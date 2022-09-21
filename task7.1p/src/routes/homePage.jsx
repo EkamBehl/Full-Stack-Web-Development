1a736d04394336d9b220c01b439e46fa0856519b
EkamBehl <62633992+EkamBehl@users.noreply.github.com>
2022-09-21 19:59:37 +1000

    Create homePage.jsx

---
+++ task7.1p/src/routes/homePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Login from '../components/Login';
+import NavBar from '../components/NavBar';
+
+function HomePage() {
+  return (
+    
+     <NavBar/>
+   
+  );
+}
+
+export default HomePage;
